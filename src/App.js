ef7701d0af92d3292ccce49a4915be38bcf6a5ed
Oleksandra-Mas <sashamasyuk0702@gmail.com>
2021-12-05 21:50:29 +0200

    deleted some things and added 1 task html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import user from './data/user.json';
+import statistics from './data/statistics.json';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Profile
+                username={user.username}
+                tag={user.tag}
+                location={user.location}
+                avatar={user.avatar}
+                stats={user.stats}
+            />
+            <Statistics title="Upload stats" stats={statistics} />
+            <Statistics stats={statistics} />
         </div>
     );
 }
